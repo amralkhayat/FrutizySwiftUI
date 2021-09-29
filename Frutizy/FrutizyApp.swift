23a95154029276595316262b12f0cee7cc949d4d
amr <amralkhayat0@gmail.com>
2021-09-29 23:04:22 +0200

    Initial Commit

---
+++ Frutizy/FrutizyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrutizyApp.swift
+//  Frutizy
+//
+//  Created by Amr Saeed on 29/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct FrutizyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
